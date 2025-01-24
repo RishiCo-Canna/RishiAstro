7613b8631328526e19a32b1867c8ac9d16998c4f
mwaxman519 <115736601+mwaxman519@users.noreply.github.com>
2025-01-24 21:29:27 +0000

    .kj

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,17 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
+import tina from "tinacms/astro";
 
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [
+    react(), 
+    tailwind(),
+    tina({
+      publicFolder: "public",
+      clientId: process.env.TINA_CLIENT_ID,
+      token: process.env.TINA_TOKEN,
+    })
+  ],
   output: 'static'
 });
---
+++ tina/config.ts
@@ -0,0 +1,51 @@
+import { defineConfig } from "tinacms";
+
+// Your hosting provider likely exposes this as an environment variable
+const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+export default defineConfig({
+  branch,
+  clientId: process.env.TINA_CLIENT_ID || "", // Get this from tina.io
+  token: process.env.TINA_TOKEN || "", // Get this from tina.io
+  build: {
+    outputFolder: "admin",
+    publicFolder: "public",
+  },
+  media: {
+    tina: {
+      mediaRoot: "images",
+      publicFolder: "public",
+    },
+  },
+  schema: {
+    collections: [
+      {
+        name: "post",
+        label: "Posts",
+        path: "src/content/posts",
+        format: "mdx",
+        fields: [
+          {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+          },
+          {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+          },
+          {
+            type: "datetime",
+            name: "publishDate",
+            label: "Publish Date",
+            required: true,
+          },
+        ],
+      },
+    ],
+  },
+});
